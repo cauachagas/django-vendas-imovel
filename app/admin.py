4a46fef8f48c4b9db1fb3569b9dcb4c09dd61043
Cauã da Silva Chagas dos Santos <caua.geof@gmail.com>
2021-05-14 21:55:21 -0300

    Registrando Categoria

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Cliente
+from app.models import Cliente, Categoria
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ("nome", "email", "data_cadastro")
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome")
