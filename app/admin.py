ebd6bd390b179bc6db1b722b6cf682894e993c64
Cauã da Silva Chagas dos Santos <caua.geof@gmail.com>
2021-05-14 23:41:07 -0300

    Adicionando campo de busca no painel admin

--- app/admin.py
+++ app/admin.py
@@ -6,16 +6,20 @@ from app.models import Cliente, Categoria, Imovel, Venda
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ("nome", "email", "data_cadastro")
-
+    search_fields = ('nome',)
+    
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ("id", "nome")
+    search_fields = ('nome',)
 
 @admin.register(Imovel)
 class ImovelAdmin(admin.ModelAdmin):
     list_display = ("id", "imagem", "categoria", "titulo", "endereco")
+    search_fields = ('titulo', "endereco")
 
 @admin.register(Venda)
 class VendaAdmin(admin.ModelAdmin):
     fields = ("categoria", "imovel", "valor", "cliente", 'corretor', 'pagamento')
     list_display = ('id', 'categoria','imovel', 'endereco', 'valor', 'pagamento', 'comissao')
+    search_fields = ("categoria", "imovel", "cliente", 'corretor', 'pagamento')
