25b3e1f1da1e0aaa47939a61f063fb0146f7eab8
Cauã da Silva Chagas dos Santos <caua.geof@gmail.com>
2021-05-14 21:52:59 -0300

    Registrando Model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Cliente
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "email", "data_cadastro")
