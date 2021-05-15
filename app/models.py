af1e145b822a7676592d3b44482f9af713032c76
Cauã da Silva Chagas dos Santos <caua.geof@gmail.com>
2021-05-14 21:52:38 -0300

    Criando Model Cliente

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cliente(models.Model):
+    '''
+    Model que representa o Cliente que será cadastrado
+    '''
+    id = models.AutoField(primary_key=True, unique=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefone = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=11)
+    data_cadastro = models.DateField(auto_now_add=True)
+    imagem = models.ImageField( blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nome}"
+
+    class Meta:
+        # definindo nome da nossa tabela
+        db_table = 'cliente'
+        # ordenando por nome
+        ordering = ['nome']
